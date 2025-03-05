f45362879bf7e0cfec503d57560f63dce8180aca
AyeshaRehan2619 <ayesharehan680@gmail.com>
2025-03-05 12:11:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,18 @@
+<!--Footer-->
+    <footer>
+        <div class="footer">
+            <div class="footer-first">
+                <h3>Ayesha's Developer Portfolio</h3>   
+            </div>
+            <div class="footer-second">
+                <ul>
+                    <li>Home</li> 
+                    <li>Work Experience</li>
+                    <li>Projects</li>
+                    <li>Contact</li>
+
+                </ul>
+            </div>
+        </div>
+        
+    </footer>
